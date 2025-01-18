de2157f1f391eba32d9fbf1a8e0b7a9c0a8187aa
Kazi Shihabul Islam <Kazi Shihabul Islam _public_kazishihabulislam.info@gmail.com>
2025-01-19 00:32:22 +0600

    Initial setup with React, DaisyUI, Tailwind CSS & Vite.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Hero from './components/Hero/Hero'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-3xl font-bold text-center py-4'>Refactoring UI</h2>
+      <Hero></Hero>
+    </>
+  )
+}
+
+export default App
